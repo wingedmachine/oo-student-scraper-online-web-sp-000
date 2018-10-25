7b204aecd83825300c915c2c4a2e6dfc73a19add
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-25 15:12:51 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,15 @@
 require 'open-uri'
+require 'Nokoiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    []
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
