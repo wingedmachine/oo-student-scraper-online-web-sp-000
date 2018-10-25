0c3b001745cc45d19083d6d381ac7e07b2c8b93a
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-25 15:34:44 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,7 +16,7 @@ class Scraper
     {
       name: student.css("h4.student-name").text,
       location: student.css("p.student-location").text,
-      profile_url: student.css("a").first
+      profile_url: student.css("a").first["href"]
     }
   end
 
