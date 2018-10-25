c79998ae548e4a97fde04977e41f51cdc705b82b
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-25 15:27:29 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,9 +5,17 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    students = Nokogiri::HTML(open(index_url)).css("div.student-card").first
+
+    students = [Nokogiri::HTML(open(index_url)).css("div.student-card").first]
+    students = student_cards.map { |student| parse_student_card(student) }
+    students.map { |student| Student.new(student) }
   end
 
+  def parse_student_card(student_card)
+    pry
+  end
+
+  def parse
   def self.scrape_profile_page(profile_url)
 
   end
